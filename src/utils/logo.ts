7db80e2e72d0892f14615c2b11d951b371a23726
vedantlavale <vedantlavale@gmail.com>
2025-11-12 00:29:54 +0530

    remove unwanted lofs

--- src/utils/logo.ts
+++ src/utils/logo.ts
@@ -6,8 +6,7 @@ import { renderFilled } from 'oh-my-logo';
  * The art is blocky to match the attached design.
  */
 export async function displayLogo(): Promise<void> {
-  const logo = await renderFilled("SHADNEX", { palette: "dawn" });
-  console.log(logo);
+  await renderFilled("SHADNEX", { palette: "dawn" });
 
   console.log(kleur.dim('            The ultimate CLI for Next.js with Shadcn UI'));
   console.log();
